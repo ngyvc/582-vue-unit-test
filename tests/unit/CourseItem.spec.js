da1b21d580c3155430f337faf383d19730d65097
Peter Ng <ngy@vaniercollege.qc.ca>
2023-08-03 17:11:31 -0400

    add testing

---
+++ tests/unit/CourseItem.spec.js
@@ -0,0 +1,24 @@
+describe('CourseItem.vue', () => {
+    it("renders the props", ()=> {
+
+    });
+
+    it("emits an add course event", () => {
+
+    });
+
+    it("emits a remove course event", () => {
+
+    });
+
+    it("displays the add button if available and it was not selected", () => {
+
+    });
+
+    it("hides the add button if not available or selected", () => {
+
+    });
+
+    
+
+});
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,10 +3,52 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    // const msg = "new message";
+    // const wrapper = shallowMount(HelloWorld, {
+    //   props: { msg },
+    // });
+    // expect(wrapper.text()).toMatch(msg);
   });
+
+  it("is true", () => {
+    expect(true).toBe(true);
+  });
+
+  it("adds 1 plus 1", () => {
+    const wrapper = shallowMount(HelloWorld);
+    expect(wrapper.vm.add(1, 1)).toBe(2);
+  });
+
+  it("shows the button when visible is true", () => {
+    const buttonText = "Button";
+    const wrapper = shallowMount(HelloWorld);
+    // expect(wrapper.text()).toBe(buttonText);
+    expect(wrapper.find("div").text()).toBe(buttonText);
+  });
+
+  it("hides the button when visible is false", async () => {
+    const buttonText = "Button";
+    const wrapper = shallowMount(HelloWorld);
+    // expect(wrapper.text()).toBe(buttonText);
+    expect(wrapper.find("div").text()).toBe(buttonText);
+    await wrapper.setData({ visible: false });
+    expect(wrapper.find("div").exists()).toBe(false);
+  });
+
+  it("emits an action when the button is clicked", async () => {
+    const wrapper = shallowMount(HelloWorld);
+    await wrapper.find("div").trigger("click");
+    expect(wrapper.emitted().action).toBeTruthy();
+    console.log(wrapper.emitted().action);
+    expect(wrapper.emitted().action[0][0]).toEqual(true);
+  });
+
+  it("changes the class of the button when clicked", async () => {
+    const wrapper = shallowMount(HelloWorld);
+    expect(wrapper.vm.isClicked).toBe(false);
+    await wrapper.find("div").trigger("click");
+    expect(wrapper.vm.isClicked).toBe(true);
+    expect(wrapper.classes()).toContain("isClicked");
+  });
+
 });
